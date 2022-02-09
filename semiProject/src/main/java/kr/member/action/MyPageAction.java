3a9ef0fa42777b44a94afbeaff51c44287278993
yuni0810 <tomatomayeh@gmail.com>
2022-02-09 23:49:16 +0900

    MyPageAction수정

--- semiProject/src/main/java/kr/member/action/MyPageAction.java
+++ semiProject/src/main/java/kr/member/action/MyPageAction.java
@@ -16,14 +16,14 @@ public class MyPageAction implements Action{
 		//세션객체 구하기 
 		HttpSession session = request.getSession();
 		//로그인된 상태에서만 myPage에 진입할수있도록 처리
-		Integer user_number = (Integer)session.getAttribute("user_num"); //회원번호를 구한다
+		Integer user_num = (Integer)session.getAttribute("user_number"); //회원번호를 구한다
 		//로그인 안 된 경우
-		if(user_number==null){//로그인 안된경우
+		if(user_num==null){//로그인 안된경우
 			return "redirect:/member/loginForm.do";
 		}		
 		//로그인 된 경우
 		MemberDAO dao = MemberDAO.getInstance();
-		MemberVO member = dao.getMember(user_number); //세션에 name: "user_number"으로 저장돼있는 value: user_num을 getMember에 넘겨 회원정보를 구해 자바빈에 담는다
+		MemberVO member = dao.getMember(user_num); //세션에 저장돼있는 user_num을 getMember에 넘겨 회원정보를 구해 자바빈에 담는다
 		
 		request.setAttribute("member", member); //"member"라는 이름으로 회원정보담긴 자바빈인 member를 저장
 		
