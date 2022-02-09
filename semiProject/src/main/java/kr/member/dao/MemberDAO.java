01e3c8f5d8688b161d71baf95640c5297e637157
yuni0810 <tomatomayeh@gmail.com>
2022-02-09 23:20:13 +0900

    dao회원상세정보

--- semiProject/src/main/java/kr/member/dao/MemberDAO.java
+++ semiProject/src/main/java/kr/member/dao/MemberDAO.java
@@ -28,8 +28,8 @@ public class MemberDAO {
 		try {
 			conn = DBUtil.getConnection();
 			conn.setAutoCommit(false);
-			sql = "select qmember_seq.nextval from dual";
 			
+			sql = "select qmember_seq.nextval from dual";			
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			if(rs.next()) {
@@ -43,8 +43,7 @@ public class MemberDAO {
 			pstmt2.executeUpdate();
 			
 			sql = "insert into qmember_detail (user_num,name,passwd,phone,zipcode,address1,address2) values (?,?,?,?,?,?,?)";
-			pstmt3 = conn.prepareStatement(sql);
-			
+			pstmt3 = conn.prepareStatement(sql);		
 			pstmt3.setInt(1, num);
 			pstmt3.setString(2, member.getName());
 			pstmt3.setString(3, member.getPasswd());
@@ -53,6 +52,7 @@ public class MemberDAO {
 			pstmt3.setString(6, member.getAddress1());
 			pstmt3.setString(7, member.getAddress2());
 			pstmt3.executeUpdate();
+			
 			conn.commit();
 			
 		}catch(Exception e) {
@@ -64,6 +64,7 @@ public class MemberDAO {
 			DBUtil.executeClose(null, pstmt3, null);
 		}
 	}
+	
 	//ID중복 체크 및 로그인 처리
 	public MemberVO checkMember(String id)throws Exception{
 		Connection conn = null;
@@ -94,7 +95,55 @@ public class MemberDAO {
 		}
 		return member;
 	}
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ		
 	//회원상세정보
+	public MemberVO getMember(int user_num)throws Exception{
+		Connection conn=null;
+		PreparedStatement pstmt =null;
+		ResultSet rs=null;
+		MemberVO member = null;
+		String sql=null;
+		
+		try {
+			//커넥션풀로부터 커넥션을 할당받음 
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM qmember m JOIN qmember_detail d "
+					+ "ON m.user_num=d.user_num WHERE m.user_num=?";	
+			
+			//PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setInt(1, user_num);
+			
+			//SQL문을 테이블에 반영하고 결과행(한가지)을 ResultSet에 담는다
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				member = new MemberVO(); //자바빈(VO)객체 생성
+								
+				member.setUser_num(rs.getInt("user_num"));
+				member.setId(rs.getString("id"));
+				member.setAuth(rs.getInt("auth"));
+				member.setPasswd(rs.getString("passwd"));
+				member.setName(rs.getString("name"));
+				member.setPhone(rs.getString("phone"));
+				member.setZipcode(rs.getString("zipcode"));
+				member.setAddress1(rs.getString("address1"));
+				member.setAddress2(rs.getString("address2"));
+				member.setReg_date(rs.getDate("reg_date"));
+			}
+		}catch(Exception e) {
+			throw new Exception(e); 
+		}finally {
+			//자원정리
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		
+		return member; 
+	}
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ	
 	//회원정보수정
 	//비밀번호수정
 	//회원탈퇴(회원정보 삭제)
