cb54a14c82b2f4ba8308f08e6855fe0d2639338e
eunnnnn1996 <eunnnnn1996@gmail.com>
2022-02-13 21:56:48 +0900

    d

--- semiProject/src/main/java/kr/order/vo/CartVO.java
+++
@@ -1,6 +0,0 @@
-package kr.order.vo;
-
-public class CartVO {
-
-}
- 
--- semiProject/src/main/java/kr/order/vo/OrderVO.java
+++ semiProject/src/main/java/kr/order/vo/OrderVO.java
@@ -1,6 +1,59 @@
 package kr.order.vo;
 
-public class OrderVO {
+import java.sql.Date;
 
+public class OrderVO {
+	private int order_num;
+	private int user_num;
+	private String item_name;
+	private int order_total;
+	private int payment;
+	private int shipping;
+	private Date reg_date;
+	
+	public int getOrder_num() {
+		return order_num;
+	}
+	public void setOrder_num(int order_num) {
+		this.order_num = order_num;
+	}
+	public int getUser_num() {
+		return user_num;
+	}
+	public void setUser_num(int user_num) {
+		this.user_num = user_num;
+	}
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public int getOrder_total() {
+		return order_total;
+	}
+	public void setOrder_total(int order_total) {
+		this.order_total = order_total;
+	}
+	public int getPayment() {
+		return payment;
+	}
+	public void setPayment(int payment) {
+		this.payment = payment;
+	}
+	public int getShipping() {
+		return shipping;
+	}
+	public void setShipping(int shipping) {
+		this.shipping = shipping;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	
+	
 }
  
