4635a49a7d169996f571b149a99414c6413d297d
yuni0810 <tomatomayeh@gmail.com>
2022-02-19 21:07:55 +0900

    관리자 구매정보수정 페이지

---
+++ semiProject/src/main/java/kr/order/action/AdminModifyFormAction.java
@@ -0,0 +1,43 @@
+package kr.order.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.controller.Action;
+import kr.order.dao.OrderDAO;
+import kr.order.vo.OrderDetailVO;
+import kr.order.vo.OrderVO;
+
+public class AdminModifyFormAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		Integer user_num = (Integer)session.getAttribute("user_number");
+		if(user_num == null) {//로그인 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
+		
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
+		if(user_auth < 3) {//관리자로 로그인하지 않은 경우
+			return "/WEB-INF/views/common/notice.jsp";	
+		}
+		
+		int order_num = Integer.parseInt(request.getParameter("order_num"));
+		
+		OrderDAO dao = OrderDAO.getInstance();
+		//주문 정보
+		OrderVO order = dao.getOrder(order_num);
+		//개별 상품 목록
+		List<OrderDetailVO> detailList = dao.getListOrderDetail(order_num);
+		
+		request.setAttribute("order", order);
+		request.setAttribute("detailList", detailList);
+		
+		return "/WEB-INF/views/order/admin_modifyForm.jsp";
+	}
+
+}
