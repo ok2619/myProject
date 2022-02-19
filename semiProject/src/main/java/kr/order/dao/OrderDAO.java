dbba290bbf56d162235bf192f97790cb3aa112c8
eunnnnn1996 <eunnnnn1996@gmail.com>
2022-02-19 17:42:12 +0900

    qe

--- semiProject/src/main/java/kr/order/dao/OrderDAO.java
+++ semiProject/src/main/java/kr/order/dao/OrderDAO.java
@@ -1,6 +1,11 @@
 package kr.order.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import kr.order.dao.OrderDAO;
+import kr.util.DBUtil;
 
 public class OrderDAO {
 	private static OrderDAO instance = new OrderDAO();
@@ -9,6 +14,39 @@ public class OrderDAO {
 	}
 	private OrderDAO() {}
 	
+	//장바구니 회원별 총 구입 금액
+	public int getTotalByMem_num(int user_number)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int total = 0;
+		
+		try {
+			//커넥션풀로부터 커넥션 할당
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT SUM(sub_total) FROM "
+					+ "(SELECT c.user_num, c.cart_count * i.price as sub_total "
+					+ "FROM qcart c JOIN qproduct i ON c.product_num = i.product_num)"
+					+ "WHERE user_num=?";
+			//PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setInt(1, user_number);
+			//SQL문을 실행해서 결과행을 ResultSet에 담음
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				total = rs.getInt(1);
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return total;
+	}
 	
 	//상품주문
 	//주문수정
--- semiProject/src/main/java/kr/product/action/CartBuyFormAction.java
+++ semiProject/src/main/java/kr/product/action/CartBuyFormAction.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
+import kr.order.dao.OrderDAO;
 import kr.product.dao.ProductDAO;
 import kr.product.vo.CartVO;
 import kr.product.vo.ProductVO;
@@ -18,12 +19,46 @@ public class CartBuyFormAction implements Action{
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
 		Integer user_number = (Integer)session.getAttribute("user_number");
+		if(user_number == null) {
+			return "redirect:/member/loginForm.do";
+		}
 		
 		ProductDAO dao = ProductDAO.getInstance();
-		List<CartVO> product = dao.cartList(user_number);
+		/*
+		 * List<CartVO> product = dao.cartList(user_number); //장바구니에서 구매 클릭시 장바구니 담긴 목록
+		 * 출력
+		 */		
+		OrderDAO orderdao = OrderDAO.getInstance();
+		int all_total = orderdao.getTotalByMem_num(user_number);
+		
+		if(all_total<=0) {
+			request.setAttribute("notice_msg", 
+					     "정상적인 주문이 아니거나 상품의 수량이 부족합니다!");
+			request.setAttribute("notice_url", 
+					    request.getContextPath()+"/product/productList.do");
+			return "/WEB-INF/views/common/alert_singleView.jsp";
+		}
 		
 		
-		request.setAttribute("product", product);
+		//장바구니에 담겨있는 상품 정보 호출
+				List<CartVO> cartList = dao.cartList(user_number);
+				for(CartVO cart : cartList) {
+					ProductDAO itemDao = ProductDAO.getInstance();
+					ProductVO item = itemDao.getProduct(cart.getProduct_num());
+					
+					
+					if(item.getStock() < cart.getCart_count()) {
+						//상품 재고 수량 부족
+						request.setAttribute("notice_msg", 
+								       "[" + item.getProduct_name() + "]재고 수량 부족으로 주문 불가");
+						request.setAttribute("notice_url", 
+								             request.getContextPath()+"/product/cartList.do");
+						return "/WEB-INF/views/common/alert_singleView.jsp";
+					}
+				}
+				
+				request.setAttribute("list", cartList);
+				request.setAttribute("all_total", all_total);
 		
 		return "/WEB-INF/views/product/cartBuyForm.jsp";
 	}
