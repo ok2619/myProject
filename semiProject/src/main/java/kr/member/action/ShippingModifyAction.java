10a8c3206ea8a3bfb2986de105fb50a183569756
eunnnnn1996 <eunnnnn1996@gmail.com>
2022-02-21 15:44:23 +0900

    z

---
+++ semiProject/src/main/java/kr/member/action/ShippingModifyAction.java
@@ -0,0 +1,38 @@
+package kr.member.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.controller.Action;
+import kr.order.dao.OrderDAO;
+import kr.order.vo.OrderDetailVO;
+import kr.order.vo.OrderVO;
+
+public class ShippingModifyAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		HttpSession session = request.getSession();
+		Integer user_number = (Integer)session.getAttribute("user_number");
+		if(user_number == null) {
+			return "redirect:/member/loginForm.do";
+		}
+		request.setCharacterEncoding("utf-8");
+		
+		int order_num = Integer.parseInt(request.getParameter("order_num"));
+
+		OrderDAO dao = OrderDAO.getInstance();
+		OrderVO order = dao.getOrder(order_num);
+		List<OrderDetailVO> detailList = dao.getListOrderDetail(order_num);
+		
+		request.setAttribute("order", order);
+		request.setAttribute("detailList", detailList);
+		return "/WEB-INF/views/member/shippingModify.jsp";
+	}
+
+}
